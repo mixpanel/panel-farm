bf8a63f160a187eb2243c123e95523f88fb86f8e
t ddddddd <webdev@cmme.org>
2016-05-19 22:34:38 -0700

    animal-button

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello world!');
+import { Component } from 'panel';
+
+import template from './animal-button.jade';
+
+document.registerElement('animal-button', class extends Component {
+  get config() {
+    return {
+      defaultState: {animals: []},
+      template,
+      helpers: {
+        addAnimal: () => this.update({animals: this.state.animals.concat(this.animal)}),
+      }
+    };
+  }
+
+  get animal() {
+    return this.getAttribute('animal');
+  }
+});
