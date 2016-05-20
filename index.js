2155cf3e6a7c6d8f2c48a46cece1c8cb4307c571
t ddddddd <webdev@cmme.org>
2016-05-19 22:56:51 -0700

    animal-farm

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hello world!');
+import { Component } from 'panel';
+
+import farmTemplate from './animal-farm.jade';
+import buttonTemplate from './animal-button.jade';
+
+document.registerElement('animal-farm', class extends Component {
+  get config() {
+    return {
+      defaultState: {
+        animals: [],
+        buttons: ['llama', 'doge'],
+      },
+      template: farmTemplate,
+    };
+  }
+});
+
+document.registerElement('animal-button', class extends Component {
+  get config() {
+    return {
+      helpers: {
+        addAnimal: () => this.update({animals: this.state.animals.concat(this.animal)}),
+      },
+      template: buttonTemplate,
+    };
+  }
+
+  get animal() {
+    return this.getAttribute('animal');
+  }
+});
