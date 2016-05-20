a2b74ef720fb056d8a9aebe7364fbd0d81b7e886
teddddd <ted@mixpanel.com>
2016-05-20 11:17:05 -0700

    farm2

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hello world!');
+import { Component } from 'panel';
+
+import buttonTemplate from './animal-button.jade';
+import farmTemplate from './animal-farm.jade';
+
+document.registerElement('animal-farm', class extends Component {
+  get config() {
+    return {
+      defaultState: {
+        buttons: ['llama', 'doge'],
+        animals: ['angry bird'],
+      },
+      template: farmTemplate,
+    };
+  }
+});
+
+document.registerElement('animal-button', class extends Component {
+  get config() {
+    return {
+      helpers: {
+        addAnimal: () => this.update({animals: this.state.animals.concat(this.animal)}),
+      },
+      template: buttonTemplate,
+    };
+  }
+
+  get animal() {
+    return this.getAttribute('animal');
+  }
+});
