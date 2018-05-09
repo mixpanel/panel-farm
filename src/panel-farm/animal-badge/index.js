1492775de28a725310676b4b8df1b37c7164b8fb
teddddd <ted@mixpanel.com>
2018-05-09 15:16:56 -0700

    some cute animals

--- src/panel-farm/animal-badge/index.js
+++ src/panel-farm/animal-badge/index.js
@@ -1,6 +1,7 @@
 import {Component} from 'panel';
 
 import template from './template.jade';
+import './styles.styl'; // TODO inline
 
 customElements.define(`animal-badge`, class extends Component {
   get config() {
