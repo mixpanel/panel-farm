884f4198c7c04c7d208f9f949c7f7ce1ecc179c0
teddddd <ted@mixpanel.com>
2018-05-08 18:11:30 -0700

    addAnimal buttons

--- src/panel-farm/view-farm/index.js
+++ src/panel-farm/view-farm/index.js
@@ -1,11 +1,15 @@
 import {Component} from 'panel';
 
 import template from './template.jade';
+import './styles.styl';
 
 customElements.define(`view-farm`, class extends Component {
   get config() {
     return {
       template,
+      helpers: {
+        addAnimal: animal => this.update({animals: [...this.state.animals, animal]}),
+      },
     };
   }
 });
